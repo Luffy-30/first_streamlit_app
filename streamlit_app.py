48dc6182516108d1b2e75ae6238ea43b1e68ae80
shriraaman <69852935+Luffy-30@users.noreply.github.com>
2023-08-23 22:42:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 # Let's put a pick list here so they can pick the fruit they want to include 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-  fruityvice_normalized pandas.json_normalize (fruityvice_response.json())
+  fruityvice_normalized= pandas.json_normalize (fruityvice_response.json())
   return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
