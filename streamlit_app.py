6e8973a1b8a086715d140313b97ee9b8f6c6d95b
shriraaman <69852935+Luffy-30@users.noreply.github.com>
2023-08-23 22:42:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # Let's put a pick list here so they can pick the fruit they want to include 
 def get_fruityvice_data(this_fruit_choice):
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-fruityvice_normalized pandas.json_normalize (fruityvice_response.json())
-return fruityvice_normalized
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized pandas.json_normalize (fruityvice_response.json())
+  return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
